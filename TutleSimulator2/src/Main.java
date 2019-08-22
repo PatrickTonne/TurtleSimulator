3bd5d886c976340b71551392931f78dd3d8e4d70
Patrick <Patrick@Patrick-PC-NEU.fritz.box>
2019-08-22 19:31:00 +0200

    imports wieder drin

--- TutleSimulator2/src/Main.java
+++ TutleSimulator2/src/Main.java
@@ -1,4 +1,13 @@
-import 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class Main extends Application{
 
 	public static void main(String[] args) {
@@ -27,7 +36,7 @@ public class Main extends Application{
 				VBox toolBox = new VBox();
 				Button button1 = new Button();
 				button1.setPrefSize(200, 200);
-				button1.setMaxWidth(1000);
+				button1.setMaxWidth(10000);
 				toolBox.getChildren().addAll(button1);
 				
 				borderpane2.setTop(toolBox);
