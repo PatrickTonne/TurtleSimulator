d502755d935ffccc2772619debf865c2c80626b1
Patrick <Patrick@Patrick-PC-NEU.fritz.box>
2019-08-23 15:20:45 +0200

    ManuBar fixed

--- TutleSimulator2/src/Main.java
+++ TutleSimulator2/src/Main.java
@@ -1,10 +1,16 @@
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,9 +25,41 @@ public class Main extends Application{
 		//Erstes Border Pane Implementierung
 				BorderPane borderpane1 = new BorderPane();
 				MenuBar menuBar = new MenuBar();
+				
 				Menu editorMenu = new Menu("Editor");
+				MenuItem neuItem = new MenuItem("Neu");
+				MenuItem öffnenItem = new MenuItem("Öffnen");
+				MenuItem kompilierenItem = new MenuItem("Kompilieren");
+				MenuItem druckenItem = new MenuItem("Drucken");
+				MenuItem beendenItem = new MenuItem("Beenden");
+				editorMenu.getItems().addAll(neuItem, öffnenItem, new SeparatorMenuItem(),kompilierenItem, druckenItem, new SeparatorMenuItem(),beendenItem);
+				
+				//Territorium Menu
 				Menu terriMenu = new Menu("Territorium");
+				MenuItem SpeichernItem = new MenuItem("Speichern");
+				MenuItem LadenItem = new MenuItem("Laden");
+				MenuItem bildSpeichernItem = new MenuItem("Als Bild speichern");
+				MenuItem druckenItem2 = new MenuItem("Drucken");
+				MenuItem gößeändernItem = new MenuItem("Größe ändern...");
+				
+				ToggleGroup terriToggleGroup = new ToggleGroup();
+				
+				RadioMenuItem schildkrötePlazierenMenuItem = new RadioMenuItem("Schildkröte plazieren");
+				schildkrötePlazierenMenuItem.setToggleGroup(terriToggleGroup);
+				RadioMenuItem salatPlazierenMenuItem = new RadioMenuItem("Salat plazieren");
+				salatPlazierenMenuItem.setToggleGroup(terriToggleGroup);
+				RadioMenuItem MauerPlazierenMenuItem = new RadioMenuItem("Mauer plazieren");
+				MauerPlazierenMenuItem.setToggleGroup(terriToggleGroup);
+				RadioMenuItem kachelLöschenMenuItem = new RadioMenuItem("Kachel löschen");
+				kachelLöschenMenuItem.setToggleGroup(terriToggleGroup);
+				
+				terriMenu.getItems().addAll(SpeichernItem, LadenItem, bildSpeichernItem, druckenItem2, gößeändernItem, new SeparatorMenuItem(), schildkrötePlazierenMenuItem,
+				salatPlazierenMenuItem, MauerPlazierenMenuItem, kachelLöschenMenuItem);
+				
+				
 				Menu hamsterMenu = new Menu("Hamster");
+				
+				
 				Menu SimulationMenu = new Menu("Simulation");
 				menuBar.getMenus().addAll(editorMenu, terriMenu, hamsterMenu,SimulationMenu);
 				borderpane1.setTop(menuBar);
---
+++ TutleSimulator2/src/Modules.java
@@ -0,0 +1,7 @@
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+
+public class Modules {
+
+	}
+
